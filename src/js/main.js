0a0302ade5a13457ea5dd536d575dd6c09d8d718
Denis Popov <matrosege@mail.ru>
2017-02-19 22:45:25 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
  */
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
-//= ../../bower_components/parallax-js/js/parallax.js
+//= ../../bower_components/parallax/parallaxjs/parallax.min.js
 //= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js
 //= ../../bower_components/mixitup/build/jquery.mixitup.min.js
 //= ../../bower_components/scrolltoid/scrolltoid.js
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
 	$(".main-head").css("min-height", $(window).height());
-	
+	$('.parallax-window').parallax({imageSrc: 'img/bg.jpg'});
 	
 });
